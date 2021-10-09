2248aabf8d111bfb36e610aa5c657de9f01bcb74
Tiago Weber <webertiagof@gmail.com>
2021-10-09 12:17:57 -0300

    Cria método apresentar na classe Diretor

--- Aula-02/exercícios-aula-2/src/main/java/Diretor.java
+++ Aula-02/exercícios-aula-2/src/main/java/Diretor.java
@@ -4,7 +4,7 @@ public class Diretor {
     private int qtdFilmesDirigidos;
     private Genero genero;
 
-    public Diretor(String nome, int idade, int qtdFilmesDirigidos) {
+    public Diretor(String nome, int idade, int qtdFilmesDirigidos, Genero genero) {
         this.nome = nome;
         this.idade = idade;
         this.qtdFilmesDirigidos = qtdFilmesDirigidos;
@@ -12,5 +12,11 @@ public class Diretor {
 
     public String getNome() {
         return nome;
+    }
+
+    public void apresentar() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Gênero: " + genero);
     }
 }
