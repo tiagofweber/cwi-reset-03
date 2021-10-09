a8fc65a79f3db0b1666d9db68d934e6aea1501df
Tiago Weber <webertiagof@gmail.com>
2021-10-09 11:05:30 -0300

    Cria classe Aplicacao

---
+++ Aula-02/exercícios-aula-2/src/main/java/Aplicacao.java
@@ -0,0 +1,26 @@
+public class Aplicacao {
+    public static void main(String args[]) {
+
+        Diretor diretor1 = new Diretor("Andrew Niccol", 45, 11);
+        Diretor diretor2 = new Diretor("Francis Laurence", 39, 15);
+
+        Filme filme1 = new Filme(
+                "O preço do amanhã.",
+                "Acusado de um assassinato, um homem deve...",
+                202,
+                2011,
+                4,
+                 diretor1);
+        Filme filme2 = new Filme(
+                "Eu sou a Lenda",
+                "Robert Neville é um brilhante cientista...",
+                190,
+                2007,
+                5,
+                diretor2
+        );
+
+        filme1.reproduzirFilme();
+        filme2.reproduzirFilme();
+    }
+}
