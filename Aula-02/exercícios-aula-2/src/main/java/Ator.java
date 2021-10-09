ffc2c74d1eb65761cfbee4074c539d6c581bc71b
Tiago Weber <webertiagof@gmail.com>
2021-10-09 12:18:55 -0300

    Cria construtor da classe Ator

--- Aula-02/exercícios-aula-2/src/main/java/Ator.java
+++ Aula-02/exercícios-aula-2/src/main/java/Ator.java
@@ -5,6 +5,13 @@ public class Ator {
     private Integer qtdOscarsVencidos;
     private Genero genero;
 
+    public Ator(String nome, Integer idade, Integer qtdOscarsVencidos, Genero genero) {
+        this.nome = nome;
+        this.idade = idade;
+        this.qtdOscarsVencidos = qtdOscarsVencidos;
+        this.genero = genero;
+    }
+
     public void apresentar() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
