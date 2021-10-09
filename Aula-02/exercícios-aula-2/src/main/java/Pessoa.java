7cb92012ec6213641398c215f09d4eb22161eedf
Tiago Weber <webertiagof@gmail.com>
2021-10-09 14:58:01 -0300

    Cria classe Pessoa

---
+++ Aula-02/exercícios-aula-2/src/main/java/Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+
+    private String nome;
+    private Integer idade;
+    private Genero genero;
+
+    public Pessoa(String nome, Integer idade, Genero genero) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+    }
+
+    public void apresentar() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Gênero: " + genero.getDescricao());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+}
