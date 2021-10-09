96b128e7a889d88ed92469ea931232a5998a6120
Tiago Weber <webertiagof@gmail.com>
2021-10-09 10:49:04 -0300

    Cria classe Diretor

--- Aula-02/exercícios-aula-2/src/main/java/Diretor.java
+++ Aula-02/exercícios-aula-2/src/main/java/Diretor.java
@@ -1,2 +1,11 @@
 public class Diretor {
+    private String nome;
+    private int idade;
+    private int qtdFilmesDirigidos;
+
+    public Diretor(String nome, int idade, int qtdFilmesDirigidos) {
+        this.nome = nome;
+        this.idade = idade;
+        this.qtdFilmesDirigidos = qtdFilmesDirigidos;
+    }
 }
