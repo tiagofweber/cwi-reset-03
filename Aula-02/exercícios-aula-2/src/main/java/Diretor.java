70325efbc4067f800b9af0da6b54e5597359da09
Tiago Weber <webertiagof@gmail.com>
2021-10-09 12:11:15 -0300

    Adiciona atributo genero na classe Diretor

--- Aula-02/exercícios-aula-2/src/main/java/Diretor.java
+++ Aula-02/exercícios-aula-2/src/main/java/Diretor.java
@@ -2,6 +2,7 @@ public class Diretor {
     private String nome;
     private int idade;
     private int qtdFilmesDirigidos;
+    private Genero genero;
 
     public Diretor(String nome, int idade, int qtdFilmesDirigidos) {
         this.nome = nome;
