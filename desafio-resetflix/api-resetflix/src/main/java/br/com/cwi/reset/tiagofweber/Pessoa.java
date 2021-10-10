d2533219623850e712334c7acbad47457587248f
Tiago Weber <webertiagof@gmail.com>
2021-10-10 11:49:57 -0300

    Cria classe Pessoa

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/Pessoa.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/Pessoa.java
@@ -2,21 +2,17 @@ package br.com.cwi.reset.tiagofweber;
 
 import java.time.LocalDate;
 
-public class Ator {
+public class Pessoa {
 
     private Integer id;
     private String nome;
     private LocalDate dataNascimento;
-    private StatusCarreira statusCarreira;
     private Integer anoInicioAtividade;
 
-    public Ator(Integer id, String nome, LocalDate dataNascimento, StatusCarreira statusCarreira, Integer anoInicioAtividade) {
+    public Pessoa(Integer id, String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
         this.id = id;
         this.nome = nome;
         this.dataNascimento = dataNascimento;
-        this.statusCarreira = statusCarreira;
         this.anoInicioAtividade = anoInicioAtividade;
     }
-
-
 }
