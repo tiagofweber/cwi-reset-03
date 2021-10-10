20b2657e185e4fb00fac4c9f93e7dd6f12eca0d2
Tiago Weber <webertiagof@gmail.com>
2021-10-10 11:53:03 -0300

    Modifica classe Diretor para estender de Pessoa

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/Diretor.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/Diretor.java
@@ -1,4 +1,10 @@
 package br.com.cwi.reset.tiagofweber;
 
-public class Diretor {
+import java.time.LocalDate;
+
+public class Diretor extends Pessoa {
+
+    public Diretor(Integer id, String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
+        super(id, nome, dataNascimento, anoInicioAtividade);
+    }
 }
