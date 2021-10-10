9029102f2d82b82aecbce165e1d8eb7211b44e40
Tiago Weber <webertiagof@gmail.com>
2021-10-10 12:13:25 -0300

    Cria classe Filme

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/Filme.java
@@ -0,0 +1,26 @@
+package br.com.cwi.reset.tiagofweber;
+
+import java.util.List;
+
+public class Filme {
+
+    private Integer id;
+    private String nome;
+    private Integer anoLancamento;
+    private String capaFilme;
+    private List<Genero> generos;
+    private Diretor diretor;
+    private List<PersonagemAtor> personagens;
+    private String resumo;
+
+    public Filme(Integer id, String nome, Integer anoLancamento, String capaFilme, List<Genero> generos, Diretor diretor, List<PersonagemAtor> personagens, String resumo) {
+        this.id = id;
+        this.nome = nome;
+        this.anoLancamento = anoLancamento;
+        this.capaFilme = capaFilme;
+        this.generos = generos;
+        this.diretor = diretor;
+        this.personagens = personagens;
+        this.resumo = resumo;
+    }
+}
