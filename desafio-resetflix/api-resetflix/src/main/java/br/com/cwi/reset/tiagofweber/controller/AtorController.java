a8a03e13f5c678854e31f570006cb3f65cfe60ff
Tiago Weber <webertiagof@gmail.com>
2021-10-17 22:07:06 -0300

    Acrescenta o Response Status Bad Request as exceções

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/AtorController.java
@@ -1,9 +1,10 @@
 package br.com.cwi.reset.tiagofweber.controller;
 
+import br.com.cwi.reset.tiagofweber.request.AtorRequest;
 import br.com.cwi.reset.tiagofweber.service.AtorService;
 import br.com.cwi.reset.tiagofweber.FakeDatabase;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/atores")
@@ -15,5 +16,9 @@ public class AtorController {
         this.atorService = new AtorService(FakeDatabase.getInstance());
     }
 
-    //demais métodos
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
+        this.atorService.criarAtor(atorRequest);
+    }
 }
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/exception/DataInvalidaException.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/exception/DataInvalidaException.java
@@ -1,5 +1,9 @@
 package br.com.cwi.reset.tiagofweber.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DataInvalidaException extends Exception {
     public DataInvalidaException(String message) {
         super(message);
