46aa5fab2cbc721bfe2ade2793d1c4c48ff1a4aa
Tiago Weber <webertiagof@gmail.com>
2021-10-17 23:04:49 -0300

    Cria o controller AtorController

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/AtorController.java
@@ -1,10 +1,13 @@
 package br.com.cwi.reset.tiagofweber.controller;
 
+import br.com.cwi.reset.tiagofweber.model.Ator;
 import br.com.cwi.reset.tiagofweber.request.AtorRequest;
 import br.com.cwi.reset.tiagofweber.service.AtorService;
 import br.com.cwi.reset.tiagofweber.FakeDatabase;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/atores")
@@ -20,5 +23,21 @@ public class AtorController {
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
         this.atorService.criarAtor(atorRequest);
+    }
+
+    // Verificar retorno List<AtorEmAtividade>
+    @GetMapping("/em-atividade")
+    public List<Ator> listarAtoresEmAtividade(@RequestParam("filtro-nome") String filtroNome) throws Exception {
+        return atorService.listarAtoresEmAtividade(filtroNome);
+    }
+
+    @GetMapping("/{id}")
+    public Ator consultarAtor(@PathVariable Integer id) throws Exception {
+        return atorService.consultarAtor(id);
+    }
+
+    @GetMapping
+    public List<Ator> consultarAtores() throws Exception {
+        return atorService.consultarAtores();
     }
 }
