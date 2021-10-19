d070f7a9a9d23050fb91758cae415a943b2a965e
Tiago Weber <webertiagof@gmail.com>
2021-10-18 21:41:40 -0300

    Cria método para cadastrar Diretor

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/DiretorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/DiretorController.java
@@ -1,8 +1,13 @@
 package br.com.cwi.reset.tiagofweber.controller;
 
 import br.com.cwi.reset.tiagofweber.FakeDatabase;
+import br.com.cwi.reset.tiagofweber.request.DiretorRequest;
 import br.com.cwi.reset.tiagofweber.service.DiretorService;
+import ch.qos.logback.core.encoder.EchoEncoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +20,9 @@ public class DiretorController {
         this.diretorService = new DiretorService(FakeDatabase.getInstance());
     }
 
-    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarDiretor(DiretorRequest diretorRequest) throws Exception {
+        this.diretorService.cadastrarDiretor(diretorRequest);
+    }
 }
