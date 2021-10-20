81721eb1e2d7f9ee09097b2f9a60c1933a143693
Tiago Weber <webertiagof@gmail.com>
2021-10-19 21:27:33 -0300

    Cria classe PersonagemRequest

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/request/PersonagemRequest.java
@@ -0,0 +1,34 @@
+package br.com.cwi.reset.tiagofweber.request;
+
+import br.com.cwi.reset.tiagofweber.model.TipoAtuacao;
+
+public class PersonagemRequest {
+
+    private Integer idAtor;
+    protected String nomePersonagem;
+    private String descricaoPersonagem;
+    private TipoAtuacao tipoAtuacao;
+
+    public PersonagemRequest(Integer idAtor, String nomePersonagem, String descricaoPersonagem, TipoAtuacao tipoAtuacao) {
+        this.idAtor = idAtor;
+        this.nomePersonagem = nomePersonagem;
+        this.descricaoPersonagem = descricaoPersonagem;
+        this.tipoAtuacao = tipoAtuacao;
+    }
+
+    public Integer getIdAtor() {
+        return idAtor;
+    }
+
+    public String getNomePersonagem() {
+        return nomePersonagem;
+    }
+
+    public String getDescricaoPersonagem() {
+        return descricaoPersonagem;
+    }
+
+    public TipoAtuacao getTipoAtuacao() {
+        return tipoAtuacao;
+    }
+}
