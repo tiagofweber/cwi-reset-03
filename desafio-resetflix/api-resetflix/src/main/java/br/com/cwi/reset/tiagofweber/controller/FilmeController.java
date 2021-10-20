d895a7d513cfbd1c80dabbabd592bc9e93d69d6a
Tiago Weber <webertiagof@gmail.com>
2021-10-19 21:59:19 -0300

    Cria classe FilmeService

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/controller/FilmeController.java
@@ -0,0 +1,17 @@
+package br.com.cwi.reset.tiagofweber.controller;
+
+import br.com.cwi.reset.tiagofweber.FakeDatabase;
+import br.com.cwi.reset.tiagofweber.service.FilmeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/filmes")
+public class FilmeController {
+
+    private FilmeService filmeService;
+
+    public FilmeController() {
+        this.filmeService = new FilmeService(FakeDatabase.getInstance());
+    }
+}
