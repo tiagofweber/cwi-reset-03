495631072556724344db8601c57f9f41cbde155e
Tiago Weber <webertiagof@gmail.com>
2021-10-19 23:07:28 -0300

    Cria método para criar personagens

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/service/PersonagemService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/service/PersonagemService.java
@@ -1,6 +1,11 @@
 package br.com.cwi.reset.tiagofweber.service;
 
 import br.com.cwi.reset.tiagofweber.FakeDatabase;
+import br.com.cwi.reset.tiagofweber.model.Ator;
+import br.com.cwi.reset.tiagofweber.model.PersonagemAtor;
+import br.com.cwi.reset.tiagofweber.request.PersonagemRequest;
+
+import java.util.List;
 
 public class PersonagemService {
 
@@ -10,5 +15,27 @@ public class PersonagemService {
         this.fakeDatabase = fakeDatabase;
     }
 
-    
+    public void criarPersonagem(PersonagemRequest personagemRequest) {
+
+        Integer novoId = fakeDatabase.recuperaPersonagens().size() + 1;
+        List<Ator> atores = fakeDatabase.recuperaAtores();
+        Ator atorEncontrado = null;
+
+        for (Ator ator : atores) {
+            if (ator.getId().equals(personagemRequest.getIdAtor())) {
+                atorEncontrado = ator;
+            }
+        }
+
+        PersonagemAtor personagemAtor = new PersonagemAtor(
+                novoId,
+                atorEncontrado,
+                personagemRequest.getNomePersonagem(),
+                personagemRequest.getDescricaoPersonagem(),
+                personagemRequest.getTipoAtuacao()
+        );
+
+        fakeDatabase.persistePersonagem(personagemAtor);
+    }
+
 }
