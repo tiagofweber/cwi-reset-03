a6fa73025a6ba1000ff5913c44d095f0e6913d6e
Tiago Weber <webertiagof@gmail.com>
2021-10-19 21:30:17 -0300

    Cria classe FilmeService

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/service/FilmeService.java
@@ -0,0 +1,14 @@
+package br.com.cwi.reset.tiagofweber.service;
+
+import br.com.cwi.reset.tiagofweber.FakeDatabase;
+
+public class FilmeService {
+
+    private FakeDatabase fakeDatabase;
+
+    public FilmeService(FakeDatabase fakeDatabase) {
+        this.fakeDatabase = fakeDatabase;
+    }
+
+    
+}
