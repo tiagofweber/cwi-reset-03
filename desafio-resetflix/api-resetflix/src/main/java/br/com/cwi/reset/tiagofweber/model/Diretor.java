41c5bbf690cc9e79b11cb0deb529c361274a6f7c
Tiago Weber <webertiagof@gmail.com>
2021-10-24 19:54:45 -0300

    Substitui classes do package model por entidades mapeadas

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/model/Diretor.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/model/Diretor.java
@@ -1,16 +1,26 @@
 package br.com.cwi.reset.tiagofweber.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Diretor  {
+@Entity
+public class Diretor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private LocalDate dataNascimento;
     private Integer anoInicioAtividade;
 
-    public Diretor(Integer id, String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
-        this.id = id;
+    public Diretor() {
+    }
+
+    public Diretor(String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
         this.anoInicioAtividade = anoInicioAtividade;
@@ -32,13 +42,32 @@ public class Diretor  {
         return anoInicioAtividade;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setAnoInicioAtividade(Integer anoInicioAtividade) {
+        this.anoInicioAtividade = anoInicioAtividade;
+    }
+
     @Override
-    public String toString() {
-        return "Diretor{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", dataNascimento=" + dataNascimento +
-                ", anoInicioAtividade=" + anoInicioAtividade +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Diretor diretor = (Diretor) o;
+        return Objects.equals(id, diretor.id) && Objects.equals(nome, diretor.nome) && Objects.equals(dataNascimento, diretor.dataNascimento) && Objects.equals(anoInicioAtividade, diretor.anoInicioAtividade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, dataNascimento, anoInicioAtividade);
     }
 }
