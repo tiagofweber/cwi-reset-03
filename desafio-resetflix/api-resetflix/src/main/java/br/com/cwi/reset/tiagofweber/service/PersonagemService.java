cf84822ff44baf05334b35d1834b3492d65ca752
Tiago Weber <webertiagof@gmail.com>
2021-10-24 22:48:40 -0300

    Refatora classe PersonagemService

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/service/PersonagemService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/service/PersonagemService.java
@@ -1,22 +1,23 @@
 package br.com.cwi.reset.tiagofweber.service;
 
-import br.com.cwi.reset.tiagofweber.FakeDatabase;
 import br.com.cwi.reset.tiagofweber.exception.IdNaoEncontradoException;
 import br.com.cwi.reset.tiagofweber.model.Ator;
 import br.com.cwi.reset.tiagofweber.model.PersonagemAtor;
+import br.com.cwi.reset.tiagofweber.repository.PersonagemAtorRepository;
 import br.com.cwi.reset.tiagofweber.request.PersonagemRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class PersonagemService {
 
-    private FakeDatabase fakeDatabase;
+    @Autowired
+    private PersonagemAtorRepository personagemAtorRepository;
+    @Autowired
     private AtorService atorService;
-
-    public PersonagemService(FakeDatabase fakeDatabase) {
-        this.fakeDatabase = fakeDatabase;
-    }
 
     public List<PersonagemAtor> criarPersonagens(List<PersonagemRequest> personagensRequest) throws Exception {
 
@@ -24,7 +25,6 @@ public class PersonagemService {
 
         for (PersonagemRequest personagemRequest : personagensRequest) {
 
-            Integer novoId = fakeDatabase.recuperaPersonagens().size() + 1;
             Ator ator = atorService.consultarAtor(personagemRequest.getIdAtor());
 
             if (ator == null) {
@@ -32,14 +32,13 @@ public class PersonagemService {
             }
 
             PersonagemAtor personagemAtor = new PersonagemAtor(
-                    novoId,
                     ator,
                     personagemRequest.getNomePersonagem(),
                     personagemRequest.getDescricaoPersonagem(),
                     personagemRequest.getTipoAtuacao()
             );
 
-            this.fakeDatabase.persistePersonagem(personagemAtor);
+            personagemAtorRepository.save(personagemAtor);
             personagens.add(personagemAtor);
         }
         return personagens;
