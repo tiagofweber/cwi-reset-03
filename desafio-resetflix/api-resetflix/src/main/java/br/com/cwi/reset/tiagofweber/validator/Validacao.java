528233f4f60b414045ebf2e5fb40d89633028748
Tiago Weber <webertiagof@gmail.com>
2021-10-27 20:57:39 -0300

    Exclui classes e métodos não utilizados

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/validator/Validacao.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tiagofweber/validator/Validacao.java
@@ -12,39 +12,6 @@ import java.util.List;
 
 public class Validacao {
 
-    public static void validarString(TipoDado tipoDado, String value) throws Exception {
-        if (value == null || value.isEmpty()) {
-            throw new CampoNaoInformadoException(tipoDado.getDescricao());
-        }
-    }
-
-    public static void validarInteger(TipoDado tipoDado, Integer num) throws Exception {
-        if (num == null) {
-            throw new CampoNaoInformadoException(tipoDado.getDescricao());
-        }
-    }
-
-    public static void validarGenero(List<Genero> generos) throws Exception {
-        if (generos == null) {
-            throw new CampoNaoInformadoException(TipoDado.GENERO.getDescricao());
-        }
-        if (generos.isEmpty()) {
-            throw new GeneroVazioException();
-        }
-        int contador = 0;
-        for (int i = 0; i < generos.size(); i++) {
-            for (int j = 0; j < generos.size(); j++) {
-                if (generos.get(i) == generos.get(j)) {
-                    contador++;
-                }
-            }
-            if (contador > 1) {
-                throw new GeneroDuplicadoException();
-            }
-            contador = 0;
-        }
-    }
-
     public static void validarPersonagens(List<PersonagemRequest> personagens) throws Exception {
         int contador = 0;
         for (int i = 0; i < personagens.size(); i++) {
